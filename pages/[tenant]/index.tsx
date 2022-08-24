e4b2aa9f6b776bb4e9199d0cac0eb2a36879a99f
Vítor Conceição <vitoralmeidavc@outlook.pt>
2022-08-24 13:52:59 +0100

    Home products

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -1,4 +1,5 @@
 import { Banner } from '../../components/Banner';
+import { ProductItem } from '../../components/ProductItem';
 import SearchInput from '../../components/SearchInput';
 import styles from '../../styles/Home.module.css';
 
@@ -33,6 +34,64 @@ const Home = () => {
           </div>
         </header>
         <Banner/>
+        <div className={styles.grid}>
+          <ProductItem
+            data={{ 
+              id: 1,
+              image: '/tmp/burguer.png',
+              categoryName: 'Traditional',
+              name: 'Texas Burguer',
+              price: '€ 8.50'
+            }}
+            mainColor="#FB9400"
+            secondColor='#FFF9F2'
+          />
+          <ProductItem
+            data={{ 
+              id: 2,
+              image: '/tmp/burguer.png',
+              categoryName: 'Traditional',
+              name: 'Texas Burguer',
+              price: '€ 8.50'
+            }}
+            mainColor="#FB9400"
+            secondColor='#FFF9F2'
+          />
+          <ProductItem
+            data={{ 
+              id: 3,
+              image: '/tmp/burguer.png',
+              categoryName: 'Traditional',
+              name: 'Texas Burguer',
+              price: '€ 8.50'
+            }}
+            mainColor="#FB9400"
+            secondColor='#FFF9F2'
+          />
+          <ProductItem
+            data={{ 
+              id: 4,
+              image: '/tmp/burguer.png',
+              categoryName: 'Traditional',
+              name: 'Texas Burguer',
+              price: '€ 8.50'
+            }}
+            mainColor="#FB9400"
+            secondColor='#FFF9F2'
+          />
+          <ProductItem
+            data={{ 
+              id: 5,
+              image: '/tmp/burguer.png',
+              categoryName: 'Traditional',
+              name: 'Texas Burguer',
+              price: '€ 8.50'
+            }}
+            mainColor="#FB9400"
+            secondColor='#FFF9F2'
+          />
+          
+        </div>
 
     </div>
   )
